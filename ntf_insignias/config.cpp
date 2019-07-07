8a96752572579c4293e29fb2118c86f6eff4c623
Tittoffer <tittoffer94@gmail.com>
2019-07-07 17:14:26 +0200

    Added ntf_insignias.

---
+++ ntf_insignias/config.cpp
@@ -0,0 +1,84 @@
+
+ //===========================================================================================================================================================
+ //========================================================================== cfgPatches =================================================================================
+ //===========================================================================================================================================================
+
+class CfgPatches {
+
+	class ntf_insignias
+	{
+		units[]={};
+		weapons[]={};
+		requiredAddons[]={};
+	};
+
+};
+//===================================================================================================================================================================
+//========================================================================== Insignia =================================================================================
+//===================================================================================================================================================================
+
+class CfgUnitInsignia
+{
+	class ntf_ins_insignia {
+		displayName = "[NTF] Norwegian Task Force"; // Name displayed in Arsenal
+		author = "Christiansen"; // Author displayed in Arsenal
+		texture = "ntf_insignia\data\ntf_insignia.paa"; // Image path
+		textureVehicle = ""; // Does nothing currently, reserved for future use
+	};
+
+	class ntf_ins_inf_wdl {
+		displayName= "[NTF] Infanteri (Skog)";
+		author = "Christiansen";
+		texture = "ntf_insignia\data\ntf_ins_inf_wdl.paa";
+		textureVehicle = "";
+	};
+	
+	class ntf_ins_inf_des {
+		displayName= "[NTF] Infanteri (Sand)";
+		author = "Christiansen";
+		texture = "ntf_insignia\data\ntf_ins_inf_des.paa";
+		textureVehicle = "";
+	};
+	
+	class ntf_ins_cav_wdl {
+		displayName= "[NTF] Kavaleri (Skog)";
+		author = "Christiansen";
+		texture = "ntf_insignia\data\ntf_ins_cav_wdl.paa";
+		textureVehicle = "";
+	};
+	
+	class ntf_ins_cav_des {
+		displayName= "[NTF] Kavaleri (Sand)";
+		author = "Christiansen";
+		texture = "ntf_insignia\data\ntf_ins_cav_des.paa";
+		textureVehicle = "";
+	};
+	
+	class ntf_ins_air_wdl {
+		displayName= "[NTF] Luft (Skog)";
+		author = "Christiansen";
+		texture = "ntf_insignia\data\ntf_ins_air_wdl.paa";
+		textureVehicle = "";
+	};
+	
+	class ntf_ins_air_des {
+		displayName= "[NTF] Luft (Sand)";
+		author = "Christiansen";
+		texture = "ntf_insignia\data\ntf_ins_air_des.paa";
+		textureVehicle = "";
+	};
+	
+	class ntf_ins_vik_wdl {
+		displayName= "[NTF] Viking (Skog)";
+		author = "Christiansen";
+		texture = "ntf_insignia\data\ntf_ins_vik_wdl.paa";
+		textureVehicle = "";
+	};
+	
+	class ntf_ins_vik_des {
+		displayName= "[NTF] Viking (Sand)";
+		author = "Christiansen";
+		texture = "ntf_insignia\data\ntf_ins_vik_des.paa";
+		textureVehicle = "";
+	};
+};
